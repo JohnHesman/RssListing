1ecdb0f1cccbc2bbfec43158b8f0206d9477d506
dotnetjohn <noreply@thewebexp.com>
2016-06-02 23:33:47 +0100

    Add project files.

---
+++ RssListing/App_Plugins/RssListing/rssListing.controller.js
@@ -0,0 +1,10 @@
+﻿angular.module("umbraco")
+    .controller("theWebExp.rssListing.grideditorcontroller",
+        function ($scope) {
+            if ($scope.control.title === undefined) {
+                $scope.control.title = "";
+            }
+            if ($scope.control.feedUrl === undefined) {
+                $scope.control.feedUrl = "";
+            }
+        });
